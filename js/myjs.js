2dd7b2469c2989e06a704a1b93649d52bcd42805
jericosaile <37940564+jericosaile@users.noreply.github.com>
2023-09-03 20:29:13 +0800

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,8 @@
+document.getElementById('btnSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+})
